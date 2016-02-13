3b49f993b3715bec10e0cafa508574e698cc3304
Roman Riznyk <roman_riznyk@epam.com>
2016-02-13 10:57:26 +0200

    set max jump speed limit

--- frog/game.js
+++ frog/game.js
@@ -4,7 +4,8 @@ window.onload = function() {
     var jumpButton;
     var platforms;
     var character;
-
+    var sky;
+    
     var jumpStartTime = 0;
 
     // game settings
@@ -15,10 +16,13 @@ window.onload = function() {
     function preload () {
         game.load.image('character', 'assets/character.png');
         game.load.image('platform', 'assets/platform.png');
+        game.load.image('sky', 'assets/background_sky.jpg');
     }
 
     function create () {
         showFps();
+        
+        sky = game.add.tileSprite(0, 0, 848, 480, 'sky');
 
         platforms = game.add.group();
         platforms = game.add.physicsGroup();
@@ -60,7 +64,15 @@ Character = function (game) {
     this.scale.setTo(0.5, 0.5);
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    this.jumpStartTime = 0; 
+    this.jumpStartTime = 0;
+    this.powerBar = new PowerBar(game, {
+            width: 100, 
+            height: 15, 
+            bgColor: '#fff',
+            color: '#FF0000'
+        });
+
+
 };
 
 Character.prototype = Object.create(Phaser.Sprite.prototype);
@@ -73,16 +85,20 @@ Character.prototype.update = function(game) {
         if (this.jumpStartTime == 0) {
             this.jumpStartTime = this.game.time.now;
         }
+        this.powerBar.setPercent((this.game.time.now - this.jumpStartTime) / 10);
     }
     if (jumpButton.isUp 
         && (this.body.onFloor() || this.body.touching.down) 
         && this.jumpStartTime > 0) {
         var speed = this.game.time.now - this.jumpStartTime;
+        speed = speed > 700 ? 700 : speed;
         this.game.debug.text(speed, 2, 58, "#00ff00");
 
         this.body.velocity.y = -speed * 1.6;
         this.body.velocity.x = speed * 0.7;
         this.jumpStartTime = 0;
+
+        this.powerBar.setPercent(0);
     }
     else if (this.body.onFloor() || this.body.touching.down) {
         this.body.velocity.x = 0;
@@ -106,3 +122,39 @@ Platform.prototype.update = function() {
         this.destroy();
     }
 };
+
+// ====================================================
+PowerBar = function (game, config) {
+    this.game = game;
+    this.config = config;
+    this.drawBackground();
+    this.drawLoadBaar();
+};
+
+PowerBar.prototype.constructor = PowerBar;
+
+PowerBar.prototype.drawBackground = function() {
+    var background = this.game.add.bitmapData(this.config.width, this.config.height);
+    background.ctx.fillStyle = this.config.bgColor;
+    background.ctx.beginPath();
+    background.ctx.rect(0, 0, this.config.width, this.config.height);
+    background.ctx.fill();
+
+    this.bgSprite = this.game.add.sprite(100, 100, background);
+};
+
+PowerBar.prototype.drawLoadBaar = function() {
+    var background = this.game.add.bitmapData(this.config.width, this.config.height);
+    background.ctx.fillStyle = this.config.color;
+    background.ctx.beginPath();
+    background.ctx.rect(0, 0, this.config.width, this.config.height);
+    background.ctx.fill();
+
+    this.barSprite = this.game.add.sprite(100, 90, background);
+};
+
+PowerBar.prototype.setPercent = function(percent) {
+    percent = percent > 100 ? 100 : percent;
+    var newWidth = (percent * this.config.width) / 100;
+    this.game.add.tween(this.barSprite).to({width: newWidth}, 10, Phaser.Easing.Linear.None, true);
+}
