6813c7e5b5a34936b446c4e0e74489001bc1efbd
Roman Riznyk <roman_riznyk@epam.com>
2016-02-15 14:52:17 +0200

    add frog animations

--- frog/game.js
+++ frog/game.js
@@ -5,23 +5,23 @@ window.onload = function() {
     var platforms;
     var character;
     var sky;
-    
-    var jumpStartTime = 0;
+    var frog;
+
+    var downFrog;
 
     // game settings
     const PLATFORM_MIN_DELAY = 2000;
     const PLATFORM_MAX_DELAY = 4000;
-    const PLATFORM_VELOCITY = -150;
+    const PLATFORM_VELOCITY = -120;
 
     function preload () {
-        game.load.image('character', 'assets/character.png');
         game.load.image('platform', 'assets/platform.png');
         game.load.image('sky', 'assets/background_sky.jpg');
+
+        game.load.spritesheet('frog', 'assets/frog.png', 512, 512, 5);
     }
 
     function create () {
-        showFps();
-        
         sky = game.add.tileSprite(0, 0, 848, 480, 'sky');
 
         platforms = game.add.group();
@@ -29,11 +29,16 @@ window.onload = function() {
 
         character = new Character(game);
         createPlatform();
+
+        frog.scale.setTo(0.2, 0.2);
+        frog.animations.add('jump');
+        frog.animations.play('jump', 15, true);
+        
+        frog.animations.add('down', [1], 15, true);
+        frog.animations.play('down', 15, true);
     }
 
     function update () {
-        game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
-  
         game.physics.arcade.collide(character, platforms);
         platforms.setAll('body.immovable', true);
     }
@@ -46,51 +51,56 @@ window.onload = function() {
             createPlatform();
         });
     }
-
-    function showFps() {
-        game.time.advancedTiming = true;
-    }
-
 };
 
 Character = function (game) {
-    Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, 'character');
+    Phaser.Sprite.call(this, game, game.world.centerX, game.world.centerY, 'frog');
+    this.animations.add('down', [1], 15, true);
+    this.animations.add('up', [1, 2, 3, 4], 15, true);
+    this.animations.add('ground', [0], 15, true);
+
     game.add.existing(this);
     game.physics.arcade.enable(this);
     this.body.collideWorldBounds = true;
     this.body.gravity.y = 2000;
-    this.anchor.setTo(0.5, 0.5);
-    this.scale.setTo(0.5, 0.5);
+    this.anchor.setTo(-0.5, -0.5);
+    this.scale.setTo(0.2, 0.2);
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     this.jumpStartTime = 0;
+    this.speed = 0;
 };
 
 Character.prototype = Object.create(Phaser.Sprite.prototype);
 Character.prototype.constructor = Character;
 
 Character.prototype.update = function(game) {
-    this.game.debug.text(this.jumpStartTime, 2, 38, "#00ff00");
-
     if (jumpButton.isDown && (this.body.onFloor() || this.body.touching.down)) {
         if (this.jumpStartTime == 0) {
             this.jumpStartTime = this.game.time.now;
+            this.animations.play('down', 15, true);
         }
     }
     if (jumpButton.isUp 
         && (this.body.onFloor() || this.body.touching.down) 
         && this.jumpStartTime > 0) {
-        var speed = this.game.time.now - this.jumpStartTime;
-        speed = speed > 700 ? 700 : speed;
-        this.game.debug.text(speed, 2, 58, "#00ff00");
-
-        this.body.velocity.y = -speed * 1.6;
-        this.body.velocity.x = speed * 0.7;
+        this.speed = this.game.time.now - this.jumpStartTime;
+        this.speed = this.speed > 700 ? 700 : this.speed;
+     
+        this.body.velocity.y = -this.speed * 1.6;
+        this.body.velocity.x = this.speed * 0.7;
         this.jumpStartTime = 0;
+
+        this.animations.play('up', 30, false);
     }
     else if (this.body.onFloor() || this.body.touching.down) {
         this.body.velocity.x = 0;
+        this.speed = 0;
     }
+    if (this.speed === 0 && !jumpButton.isDown) {
+        this.animations.play('ground', 10, true);
+    }
+
 };
 
 // ====================================================
